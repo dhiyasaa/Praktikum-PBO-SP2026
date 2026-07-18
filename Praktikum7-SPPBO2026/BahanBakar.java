197b14a3cb9822499a27b2c9674275ca40b7c766
dhiyasaa <145088218+dhiyasaa@users.noreply.github.com>
2026-07-19 01:59:39 +0700

    Delete Praktikum7-SPPBO2026 directory

--- Praktikum7-SPPBO2026/BahanBakar.java
+++
@@ -1,7 +0,0 @@
-public interface BahanBakar {
-    String jenisBahanBakar();
-
-    default void infoKonsumsi() {
-        System.out.println("Info Konsumsi: Konsumsi bahan bakar tergantung kapasitas mesin");
-    }
-}
--- Praktikum7-SPPBO2026/TransportasiUmum.java
+++
@@ -1,3 +0,0 @@
-public interface TransportasiUmum extends BahanBakar {
-    int kapasitasPenumpang();
-}
